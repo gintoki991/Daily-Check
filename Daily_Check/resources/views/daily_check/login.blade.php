e036f39e7c556feece7e70343ee7dee5c356d2dc
gintoki <midoir041206@gmail.com>
2024-05-10 12:44:38 +0900

    Livewire インストール

---
+++ Daily_Check/resources/views/daily_check/login.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+  <link rel="stylesheet" href="./global.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Alfa Slab One:wght@400&display=swap" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Alice:wght@400&display=swap" />
+</head>
+
+<body>
+  <div class="w-full relative bg-steelblue overflow-hidden flex flex-col items-start justify-start pt-[9px] pb-[292px] pr-2.5 pl-[13px] box-border gap-[212px] leading-[normal] tracking-[normal]">
+    <header class="self-stretch flex flex-col items-end justify-start gap-[24px] max-w-full text-center text-base text-white font-alfa-slab-one">
+      <div class="self-stretch flex flex-row items-end justify-between gap-[20px]">
+        <div class="w-[52px] flex flex-col items-start justify-end pt-0 px-0 pb-px box-border">
+          <img class="self-stretch h-[17px] relative rounded-xl max-w-full overflow-hidden shrink-0 object-cover" loading="lazy" alt="" src="/public/image-12@2x.png" />
+        </div>
+        <img class="h-3.5 w-[75px] relative rounded-xl object-cover" loading="lazy" alt="" src="/public/image-12-1@2x.png" />
+      </div>
+      <div class="self-stretch flex flex-row items-start justify-end py-0 pr-[14.8px] pl-[17px] box-border max-w-full">
+        <div class="flex-1 flex flex-row items-start justify-between max-w-full gap-[20px]">
+          <img class="h-[18.7px] w-[11.2px] relative object-contain" loading="lazy" alt="" src="/public/group-5@2x.png" />
+
+          <div class="w-[122.2px] flex flex-col items-start justify-start py-0 pr-[5.2px] pl-0 box-border">
+            <div class="self-stretch relative">ログイン画面</div>
+          </div>
+          <div class="flex flex-col items-start justify-start pt-px px-0 pb-0">
+            <img class="w-[11.2px] h-[19.7px] relative object-contain" loading="lazy" alt="" src="/public/group-3@2x.png" />
+          </div>
+        </div>
+      </div>
+    </header>
+    <section class="self-stretch flex flex-row items-start justify-start py-0 pr-[13px] pl-2.5 box-border max-w-full text-center text-base text-white font-alice">
+      <div class="flex-1 flex flex-col items-end justify-start gap-[13px] max-w-full">
+        <div class="self-stretch flex flex-col items-start justify-start gap-[19.5px] max-w-full">
+          <button class="cursor-pointer py-[3px] px-[17px] bg-gray self-stretch rounded-3xs box-border flex flex-row items-start justify-start max-w-full border-[1px] border-solid border-darkgray">
+            <div class="h-11 w-[327px] relative rounded-3xs bg-gray box-border hidden max-w-full border-[1px] border-solid border-darkgray"></div>
+            <div class="h-[34px] flex-1 relative text-base font-alice text-black text-center flex items-center justify-center z-[1]">
+              アカウント名を入力してください
+            </div>
+          </button>
+          <button class="cursor-pointer pt-4 px-[17px] pb-2 bg-gray self-stretch rounded-3xs box-border flex flex-row items-start justify-start max-w-full border-[1px] border-solid border-darkgray">
+            <div class="h-[62px] w-[327px] relative rounded-3xs bg-gray box-border hidden max-w-full border-[1px] border-solid border-darkgray"></div>
+            <div class="h-[34px] flex-1 relative text-base font-alice text-black text-center flex items-center justify-center z-[1]">
+              パスワードを入力してください
+            </div>
+          </button>
+          <div class="self-stretch flex flex-row items-start justify-start py-0 pr-0 pl-0.5 box-border max-w-full">
+            <button class="cursor-pointer py-[3px] px-5 bg-darksalmon flex-1 rounded-xl box-border flex flex-row items-start justify-center max-w-full border-[1px] border-solid border-darksalmon">
+              <div class="h-11 w-[327px] relative rounded-xl bg-darksalmon box-border hidden max-w-full border-[1px] border-solid border-darksalmon"></div>
+              <div class="h-[34px] w-[130px] relative text-base font-alice text-white text-center flex items-center justify-center shrink-0 z-[1]">
+                ログイン
+              </div>
+            </button>
+          </div>
+        </div>
+        <div class="self-stretch flex flex-row items-start justify-end py-0 pr-[18px] pl-5">
+          <div class="h-[34px] flex-1 relative flex items-center justify-center">
+            パスワードを忘れましたか？
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</body>
+
+</html>
