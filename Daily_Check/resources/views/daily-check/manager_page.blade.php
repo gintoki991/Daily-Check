15a81391c7e25ea4d4d65e33fc5bc34b3dda2d2c
gintoki <midoir041206@gmail.com>
2024-08-29 23:35:26 +0900

    ダッシュボードを整形

--- Daily_Check/resources/views/daily-check/manager_page.blade.php
+++ Daily_Check/resources/views/daily-check/manager_page.blade.php
@@ -10,37 +10,36 @@
     <section class="self-stretch flex items-center flex items-center justify-center min-h-screen text-center text-[1rem] text-black font-alice">
       <div class="self-stretch flex flex-col items-center justify-start gap-[0.812rem] max-w-full">
 
-        <!-- ホームボタン，従業員を管理するボタン，現場を管理するボタン -->
-        <!-- Navigation Links -->
+        <!-- ナビゲーションボタン -->
         <div class="flex flex-col items-center space-y-4 sm:-my-px sm:ms-10 sm:flex-row sm:space-y-0 sm:space-x-8">
           <x-nav-link :href="route('home')" :active="request()->routeIs('home')" wire:navigate class="no-underline">
             <button type="button" class="w-full max-w-xs py-3 px-4 inline-flex justify-center items-center text-sm font-medium rounded-lg border border-transparent bg-customGreen text-gray-900 hover:bg-teal-600 hover:text-white focus:outline-none focus:bg-teal-600 disabled:opacity-50 disabled:pointer-events-none cursor-pointer">
-              {{ __('ホ ー ム') }}
+              {{ __('ホーム') }}
             </button>
           </x-nav-link>
           <x-nav-link :href="route('site.management')" :active="request()->routeIs('dashboard')" wire:navigate class="no-underline">
-            <button type="button" class="w-full max-w-xs py-3 px-4 inline-flex justify-center items-center text-sm font-medium rounded-lg border border-transparent bg-yellow-100 text-yellow-800 hover:bg-yellow-200 focus:outline-none focus:bg-yellow-200 disabled:opacity-50 disabled:pointer-events-none dark:text-yellow-500 dark:bg-yellow-800/30 dark:hover:bg-yellow-800/20 dark:focus:bg-yellow-800/20 cursor-pointer">
-              {{ __('現場 管理') }}
+            <button type="button" class="w-full max-w-xs py-3 px-4 inline-flex justify-center items-center text-sm font-semibold rounded-lg border border-transparent bg-yellow-500 text-white hover:bg-yellow-600 focus:outline-none focus:bg-yellow-600 disabled:opacity-50 disabled:pointer-events-none cursor-pointer">
+              {{ __('現場管理') }}
             </button>
           </x-nav-link>
           <x-nav-link :href="route('employee.management')" :active="request()->routeIs('ReportCreating')" wire:navigate class="no-underline">
-            <button type="button" class="w-full max-w-xs py-3 px-4 inline-flex justify-center items-center text-sm font-medium rounded-lg border border-transparent bg-yellow-100 text-yellow-800 hover:bg-yellow-200 focus:outline-none focus:bg-yellow-200 disabled:opacity-50 disabled:pointer-events-none dark:text-yellow-500 dark:bg-yellow-800/30 dark:hover:bg-yellow-800/20 dark:focus:bg-yellow-800/20 cursor-pointer">
+            <button type="button" class="w-full max-w-xs py-3 px-4 inline-flex justify-center items-center text-sm font-semibold rounded-lg border border-transparent bg-yellow-500 text-white hover:bg-yellow-600 focus:outline-none focus:bg-yellow-600 disabled:opacity-50 disabled:pointer-events-none cursor-pointer">
               {{ __('従業員管理') }}
             </button>
           </x-nav-link>
           <x-nav-link :href="route('workers.arrangement')" :active="request()->routeIs('dashboard')" wire:navigate class="no-underline">
-            <button type="button" class="w-full max-w-xs py-3 px-4 inline-flex justify-center items-center text-sm font-medium rounded-lg border border-transparent bg-yellow-100 text-yellow-800 hover:bg-yellow-200 focus:outline-none focus:bg-yellow-200 disabled:opacity-50 disabled:pointer-events-none dark:text-yellow-500 dark:bg-yellow-800/30 dark:hover:bg-yellow-800/20 dark:focus:bg-yellow-800/20 cursor-pointer">
+            <button type="button" class="w-full max-w-xs py-3 px-4 inline-flex justify-center items-center text-sm font-semibold rounded-lg border border-transparent bg-yellow-500 text-white hover:bg-yellow-600 focus:outline-none focus:bg-yellow-600 disabled:opacity-50 disabled:pointer-events-none cursor-pointer">
               {{ __('スケジュール') }}
             </button>
           </x-nav-link>
           <x-nav-link :href="route('photoListManagement')" :active="request()->routeIs('photoListManagement')" wire:navigate class="no-underline">
-            <button type="button" class="w-full max-w-xs py-3 px-4 inline-flex justify-center items-center text-sm font-medium rounded-lg border border-transparent bg-yellow-100 text-yellow-800 hover:bg-yellow-200 focus:outline-none focus:bg-yellow-200 disabled:opacity-50 disabled:pointer-events-none dark:text-yellow-500 dark:bg-yellow-800/30 dark:hover:bg-yellow-800/20 dark:focus:bg-yellow-800/20 cursor-pointer">
-              {{ __('写真 管理') }}
+            <button type="button" class="w-full max-w-xs py-3 px-4 inline-flex justify-center items-center text-sm font-semibold rounded-lg border border-transparent bg-yellow-500 text-white hover:bg-yellow-600 focus:outline-none focus:bg-yellow-600 disabled:opacity-50 disabled:pointer-events-none cursor-pointer">
+              {{ __('写真管理') }}
             </button>
           </x-nav-link>
           <x-nav-link :href="route('documentListManagement')" :active="request()->routeIs('documentListManagement')" wire:navigate class="no-underline">
-            <button type="button" class="w-full max-w-xs py-3 px-4 inline-flex justify-center items-center text-sm font-medium rounded-lg border border-transparent bg-yellow-100 text-yellow-800 hover:bg-yellow-200 focus:outline-none focus:bg-yellow-200 disabled:opacity-50 disabled:pointer-events-none dark:text-yellow-500 dark:bg-yellow-800/30 dark:hover:bg-yellow-800/20 dark:focus:bg-yellow-800/20 cursor-pointer">
-              {{ __('書類 管理') }}
+            <button type="button" class="w-full max-w-xs py-3 px-4 inline-flex justify-center items-center text-sm font-semibold rounded-lg border border-transparent bg-yellow-500 text-white hover:bg-yellow-600 focus:outline-none focus:bg-yellow-600 disabled:opacity-50 disabled:pointer-events-none cursor-pointer">
+              {{ __('書類管理') }}
             </button>
           </x-nav-link>
         </div>
